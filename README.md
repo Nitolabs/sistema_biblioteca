bfeec1072fd5742aeab66428b1a58f944d727c06
Dominic Castillo <dominiccastillo.ag@gmail.com>
2026-07-09 17:10:42 -0600

    fix: conexion.py esta arreglada (no thread-safe) .No se haran multiples consultas

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Proyecto realizar con la finalidad de estudiar POO, MVC y base de datos a un niv
 
 ## Detalles del proyecto
 
-Versiones de softwares ocupados:
+Versiones de softwares ocupados:  
     - Python 3.14.6  
     - mysql-connector-python 9.7.0  
     - MariaDB 12.3.2  
--- database/conexion.py
+++ database/conexion.py
@@ -2,12 +2,32 @@ import mysql.connector
 
 class dataBase:
 
+    # Metodo __init__ de la clase
+    def __init__(self):
+        self.conexion = None
+
     # Metodo connectionDb, inicializa la conexion a la db biblioteca
-    def connectionDb():
-        database = mysql.connector.connect(
-            host = "localhost",
-            user = "root",
-            password = "",
-            database = "biblioteca"
-        )
+    def conexionDb(self):
+
+        if self.conexion is None or not self.conexion.is_connected():
+            self.conexion = mysql.connector.connect(
+                host = "localhost",
+                user = "root",
+                password = "",
+                database = "biblioteca"
+            )
+
+        return self.conexion
+
+    # Metodo cursor, generara el cursor que utilizara el sistema universalmente
+    def cursor(self):
+
+        return self.conexionDb().cursor()
+
+    # Metodo para cerrar conexión de la DB
+
+    def cerrarConexion(self):
+
+        if self.conexion is not None and self.conexion.is_connected():
+            self.conexion.close()
 
